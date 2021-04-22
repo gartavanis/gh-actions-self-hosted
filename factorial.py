6d4f66f91962ab8297372cd95df8ab95693028d3
David Luet <luet@princeton.edu>
2021-04-22 15:54:03 -0400

    Fix error

--- factorial.py
+++ factorial.py
@@ -7,7 +7,7 @@ def recur_factorial(n):
    if n == 1:
        return n
    else:
-       return 2*n*recur_factorial(n-1)
+       return n*recur_factorial(n-1)
 
 if __name__ == '__main__':
     arg = sys.argv
